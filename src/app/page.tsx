29ce976141d7e7d1cea8edc293f09bde761d7e9c
a.suresh.gadge <a.suresh.gadge@accenture.com>
2025-04-23 17:06:05 +0530

    homepage changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { getHomepageContent } from "../utilities/getHomePageContent";
+import Banner from "@/components/molecules/Banner/Banner";
+import BannerCarousel from "@/components/molecules/BannerCarousel/BannerCarousel";
+import GetTheLookBanner from "@/components/molecules/GetTheLookBanner/GetTheLookBanner";
+import ProductImageCarousel from "@/components/organisms/ProductImageCarousel/ProductImageCarousel";
 
-export default function Home() {
+export const revalidate = 60;
+
+export default async function Home() {
+  const { banners, products } = await getHomepageContent();
+  //const isPromoEnabled = await enablePromoFlag();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-           nEW PROJECT <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Banner
+        title={banners[0]?.title}
+        buttonText={banners[0]?.buttonText}
+        description={banners[0]?.description}
+        backgroundImage={banners[0]?.backgroundImage}
+        alignment={banners[0]?.alignment}
+        buttonLink={banners[0]?.buttonLink}
+      />
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <ProductImageCarousel productData={products} cardsPerRow={3} />
+      <GetTheLookBanner />
+      <BannerCarousel />
+      
+      <Banner
+        title={banners[1]?.title}
+        buttonText={banners[1]?.buttonText}
+        description={banners[1]?.description}
+        backgroundImage={banners[1]?.backgroundImage}
+        alignment={banners[1]?.alignment}
+        buttonLink={banners[1]?.buttonLink}
+      />
+      <Banner
+        title={banners[2]?.title}
+        buttonText={banners[2]?.buttonText}
+        description={banners[2]?.description}
+        backgroundImage={banners[2]?.backgroundImage}
+        alignment={banners[2]?.alignment}
+        buttonLink={banners[2]?.buttonLink}
+      />
+    </>
   );
 }
--- src/components/molecules/GetTheLookBanner/GetTheLookBanner.tsx
+++ src/components/molecules/GetTheLookBanner/GetTheLookBanner.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './GetTheLookBanner.module.css';
 import Typography from '../../atomic/Typography/Typography';

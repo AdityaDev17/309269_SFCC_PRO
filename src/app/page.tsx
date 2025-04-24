0739715085be383cc9e8f55a36606d5fd225bc46
a.suresh.gadge <a.suresh.gadge@accenture.com>
2025-04-24 07:13:00 +0530

    review comments addressed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,46 @@
-import { getHomepageContent } from "../utilities/getHomePageContent";
 import Banner from "@/components/molecules/Banner/Banner";
 import BannerCarousel from "@/components/molecules/BannerCarousel/BannerCarousel";
 import GetTheLookBanner from "@/components/molecules/GetTheLookBanner/GetTheLookBanner";
 import ProductImageCarousel from "@/components/organisms/ProductImageCarousel/ProductImageCarousel";
+import ProductCardBanner from "@/components/molecules/ProductCardBanner/ProductCardBanner";
+import { homepageBanners, homepageProducts } from "../common/constant"
 
 export const revalidate = 60;
 
 export default async function Home() {
-  const { banners, products } = await getHomepageContent();
-  //const isPromoEnabled = await enablePromoFlag();
 
   return (
     <>
       <Banner
-        title={banners[0]?.title}
-        buttonText={banners[0]?.buttonText}
-        description={banners[0]?.description}
-        backgroundImage={banners[0]?.backgroundImage}
-        alignment={banners[0]?.alignment}
-        buttonLink={banners[0]?.buttonLink}
+        title={homepageBanners[0]?.title}
+        buttonText={homepageBanners[0]?.buttonText}
+        description={homepageBanners[0]?.description}
+        backgroundImage={homepageBanners[0]?.backgroundImage}
+        alignment={homepageBanners[0]?.alignment}
+        buttonLink={homepageBanners[0]?.buttonLink}
       />
-
-      <ProductImageCarousel productData={products} cardsPerRow={3} />
+      
+      <ProductImageCarousel productData={homepageProducts} cardsPerRow={3} />
       <GetTheLookBanner />
+      <ProductCardBanner />
       <BannerCarousel />
       
       <Banner
-        title={banners[1]?.title}
-        buttonText={banners[1]?.buttonText}
-        description={banners[1]?.description}
-        backgroundImage={banners[1]?.backgroundImage}
-        alignment={banners[1]?.alignment}
-        buttonLink={banners[1]?.buttonLink}
+        title={homepageBanners[1]?.title}
+        buttonText={homepageBanners[1]?.buttonText}
+        description={homepageBanners[1]?.description}
+        backgroundImage={homepageBanners[1]?.backgroundImage}
+        alignment={homepageBanners[1]?.alignment}
+        buttonLink={homepageBanners[1]?.buttonLink}
       />
+
       <Banner
-        title={banners[2]?.title}
-        buttonText={banners[2]?.buttonText}
-        description={banners[2]?.description}
-        backgroundImage={banners[2]?.backgroundImage}
-        alignment={banners[2]?.alignment}
-        buttonLink={banners[2]?.buttonLink}
+        title={homepageBanners[2]?.title}
+        buttonText={homepageBanners[2]?.buttonText}
+        description={homepageBanners[2]?.description}
+        backgroundImage={homepageBanners[2]?.backgroundImage}
+        alignment={homepageBanners[2]?.alignment}
+        buttonLink={homepageBanners[2]?.buttonLink}
       />
     </>
   );

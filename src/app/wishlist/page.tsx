4808c1dc777f283bdb17f786f94423a1ed269e1c
Saha <shobhit.saha@accenture.com>
2025-04-24 17:19:02 +0530

    Wishlist toast and empty list D3

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import React from "react";
 import ButtonList from "./component";
 import styles from "./wishlist.module.css";
 import Breadcrumbs from "@/components/atomic/Breadcrumbs/Breadcrumbs";
 import Typography from "@/components/atomic/Typography/Typography";
 import ProductImageCarousel from "@/components/organisms/ProductImageCarousel/ProductImageCarousel";
+import ErrorComponent from "@/components/molecules/ErrorComponent/ErrorComponent";
+import { toast, Toaster } from "@/components/molecules/Toast/Toast";
 
 const categoryList = ["Perfume", "Jewellery", "Brush Set"];
 const productData = [
@@ -66,9 +70,19 @@ const productData = [
   },
 ];
 
+// const productData2 = []; [If wishlist is empty]
+
 function page() {
-  return (
-    <section className={styles.container}>
+  return productData.length === 0 ? (
+    <ErrorComponent
+      errImg="./images/wishlistEmpty.svg"
+      imgHeight={205}
+      imgWidth={216}
+      text1="Your wishlist is empty!"
+      buttonText="CONTINUE SHOPPING"
+    />
+  ) : (
+    <div className={styles.container}>
       <Breadcrumbs
         breadcrumbItems={[{ label: "Home", href: "/" }, { label: "Wishlist" }]}
         breadcrumbSeparator="/slash.svg"
@@ -80,15 +94,23 @@ function page() {
         label="WISHLIST"
       />
       <ButtonList buttonNames={categoryList} />
-      <ProductImageCarousel
-        cardsPerRow={4}
-        width={325}
-        productData={productData}
-        alignment="alignStart"
-        withPagination={false}
-        moveToBag={true}
-      />
-    </section>
+      <div style={{ width: "auto" }}>
+        <ProductImageCarousel
+          width="100%"
+          productData={productData}
+          alignment="alignStart"
+          withPagination={false}
+          moveToBag={true}
+          onMoveToBag={() => {
+            toast("Item has been moved to your Bag!", {
+              className: `${styles.Toast} ${styles.ToastSuccess}`,
+              unstyled: true,
+            });
+          }}
+        />
+      </div>
+      <Toaster />
+    </div>
   );
 }
 

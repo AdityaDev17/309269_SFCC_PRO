248c749a8bf3456e254cde637c283472415e0a3a
kavya.kovili@accenture.com <kavya.kovili@accenture.com>
2025-04-24 12:10:10 +0530

    Removed Const

--- src/app/product-details/[id]/page.tsx
+++ src/app/product-details/[id]/page.tsx
@@ -4,62 +4,14 @@ import ProductImageCarousel from "@/components/organisms/ProductImageCarousel/Pr
 import Banner from "@/components/molecules/Banner/Banner";
 import Typography from "@/components/atomic/Typography/Typography";
 import Breadcrumbs from "@/components/atomic/Breadcrumbs/Breadcrumbs";
+import { relatedProducts } from "@/common/constant";
 export default async function ProductDetailsPage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const products = [
-    {
-      productImage: "/images/product.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product1.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product3.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product1.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product2.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product1.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-    {
-      productImage: "/images/product2.svg",
-      productTitle: "Product 1",
-      bagPrice: "200",
-      currency: "$",
-    },
-  ];
+
   console.log("getProductId", id);
   return (
     <section className={styles.layout}>
@@ -82,7 +34,7 @@ export default async function ProductDetailsPage({
         />
         <section>
           <ProductImageCarousel
-            productData={products}
+            productData={relatedProducts}
             cardsPerRow={4}
             width={295}
             withPagination={true}
--- src/common/constant.ts
+++ src/common/constant.ts
@@ -53,6 +53,57 @@ export const productDetails = {
   ],
 };
 
+export const relatedProducts = [
+  {
+    productImage: "/images/product.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product1.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product3.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product1.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product2.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product1.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+  {
+    productImage: "/images/product2.svg",
+    productTitle: "Product 1",
+    bagPrice: "200",
+    currency: "$",
+  },
+];
+
 export const colorData = [
   { hex: "#8B0000", name: "Dark Red" },
   { hex: "#FF0000", name: "Red" },

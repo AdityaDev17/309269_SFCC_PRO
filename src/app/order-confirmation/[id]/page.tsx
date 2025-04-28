de75566bbb27fbc13f6e0e94f73785971f852d72
kavya.kovili@accenture.com <kavya.kovili@accenture.com>
2025-04-28 15:24:15 +0530

    Added Breadcrumbs

--- src/app/order-confirmation/[id]/page.tsx
+++ src/app/order-confirmation/[id]/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import OrderConfimation from "./component";
 import Banner from "@/components/molecules/Banner/Banner";
+import Breadcrumbs from "@/components/atomic/Breadcrumbs/Breadcrumbs";
+import styles from "./page.module.css"
 
 export default function OrderConfirmationPage() {
   return (
-    <>
+    <section className={`${styles.layout} ${styles.pageLayout}`}>
+      <Breadcrumbs
+        breadcrumbItems={[
+          { label: "Home", href: "/" },
+          { label: "Order Confirmation" },
+        ]}
+      />
       <Banner
         title="THANK YOU FOR PLACING YOUR ORDER!"
         textColor="#000"
@@ -15,6 +23,6 @@ export default function OrderConfirmationPage() {
         alignment="center-center"
       />
       <OrderConfimation />
-    </>
+    </section>
   );
 }

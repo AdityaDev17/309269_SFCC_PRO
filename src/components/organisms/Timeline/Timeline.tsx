0f58e9135b059b32e3607fda0b507155bdb1090e
shobhit-saha_accent <shobhit.accenture.com>
2025-05-11 22:37:47 +0530

    Residual components

--- src/components/organisms/Timeline/Timeline.tsx
+++ src/components/organisms/Timeline/Timeline.tsx
@@ -32,66 +32,66 @@ const Timeline = ({ steps, currentStep, complete = false }: TimelineProps) => {
 		currentStep = 1;
 	}
 	return (
-    <div className={styles.container}>
-      <Typography
-        type="Label"
-        variant={3}
-        fontWeight="semibold"
-        label="Shipping Details"
-      />
-      <div className={styles.progress}>
-        {
-          // eslint-disable-next-line react/no-array-index-key
-          steps.map((step, i) => {
-            // eslint-disable-next-line react/no-array-index-key
-            const isComplete = i + 1 <= currentStep || complete;
+		<div className={styles.container}>
+			<Typography
+				type="Label"
+				variant={3}
+				fontWeight="semibold"
+				label="Shipping Details"
+			/>
+			<div className={styles.progress}>
+				{
+					// eslint-disable-next-line react/no-array-index-key
+					steps.map((step, i) => {
+						// eslint-disable-next-line react/no-array-index-key
+						const isComplete = i + 1 <= currentStep || complete;
 
-            const itemClass = `${styles.stepItem} ${isComplete ? styles.complete : ""} ${i === 0 ? styles.firstStep : ""}`;
-            // styles.stepItem +
-            // " " +
-            // (isComplete ? styles.complete : "") +
-            // " " +
-            // (i === 0 ? styles.firstStep : "");
+						const itemClass = `${styles.stepItem} ${isComplete ? styles.complete : ""} ${i === 0 ? styles.firstStep : ""}`;
+						// styles.stepItem +
+						// " " +
+						// (isComplete ? styles.complete : "") +
+						// " " +
+						// (i === 0 ? styles.firstStep : "");
 
-            return (
-              <div key={step.header} className={itemClass}>
-                <div className={styles.left}>
-                  {i !== 0 && <div className={styles.verticalLine} />}
-                  <div className={styles.step}>
-                    {isComplete ? <TiTick size={24} /> : i + 1}
-                  </div>
-                </div>
-                <div className={styles.statusText}>
-                  <Typography
-                    type="Body"
-                    variant={2}
-                    fontWeight="semibold"
-                    label={step.header}
-                    color="#4F4B53"
-                  />
-                  <Typography
-                    type="Body"
-                    variant={3}
-                    fontWeight="regular"
-                    label={step.text && step.text}
-                    color="#4F4B53"
-                  />
-                </div>
-              </div>
-            );
-          })
-        }
-      </div>
-      <Button style={{ width: "100%", marginTop: "15px" }}>
-        <Typography
-          type="Body"
-          variant={2}
-          fontWeight="regular"
-          label="TRACK PACKAGE"
-        />
-      </Button>
-    </div>
-  );
+						return (
+							<div key={step.header} className={itemClass}>
+								<div className={styles.left}>
+									{i !== 0 && <div className={styles.verticalLine} />}
+									<div className={styles.step}>
+										{isComplete ? <TiTick size={24} /> : i + 1}
+									</div>
+								</div>
+								<div className={styles.statusText}>
+									<Typography
+										type="Body"
+										variant={2}
+										fontWeight="semibold"
+										label={step.header}
+										color="#4F4B53"
+									/>
+									<Typography
+										type="Body"
+										variant={3}
+										fontWeight="regular"
+										label={step.text && step.text}
+										color="#4F4B53"
+									/>
+								</div>
+							</div>
+						);
+					})
+				}
+			</div>
+			<Button style={{ width: "100%", marginTop: "15px" }}>
+				<Typography
+					type="Body"
+					variant={2}
+					fontWeight="regular"
+					label="TRACK PACKAGE"
+				/>
+			</Button>
+		</div>
+	);
 };
 
 export default Timeline;

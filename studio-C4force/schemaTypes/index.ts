488386a17ee74e86b760bff5ad6b2f6140f5000d
Sakshi Gautam <73688804+sakshi1801@users.noreply.github.com>
2025-07-08 15:54:05 +0530

    Structure builder

--- studio-C4force/schemaTypes/index.ts
+++ studio-C4force/schemaTypes/index.ts
@@ -1,22 +1,24 @@
-import banner from "./banner";
-import homePage from "./homePage";
-import landingPage from "./landingPage";
-import mediaGroup from "./mediaGroup";
-import product from "./product";
-import variant from "./variant";
-import campaign from "./campaign";
-import test from "node:test";
-import article from "./article";
-import testimonial from "./testimonial";
-import bannerSection from "./sections/bannerSection";
-import editorialSection from "./sections/editorialSection";
-import testimonialSection from "./sections/testimonialSection";
-import productCarouselSection from "./sections/productCarouselSection";
-import hero from "./modules/hero";
-import twoColumn from "./modules/twoColumn";
+import banner from './banner'
+import homePage from './homePage'
+import landingPage from './landingPage'
+import mediaGroup from './mediaGroup'
+import product from './product'
+import variant from './variant'
+import campaign from './campaign'
+import test from 'node:test'
+import article from './article'
+import testimonial from './testimonial'
+import bannerSection from './sections/bannerSection'
+import editorialSection from './sections/editorialSection'
+import testimonialSection from './sections/testimonialSection'
+import productCarouselSection from './sections/productCarouselSection'
+import hero from './modules/hero'
+import twoColumn from './modules/twoColumn'
+import siteSettings from './siteSettings'
 
 export const schemaTypes = [
   //Documents
+  siteSettings,
   product,
   variant,
   campaign,
@@ -34,4 +36,4 @@ export const schemaTypes = [
   //Components
   hero,
   twoColumn,
-];
+]

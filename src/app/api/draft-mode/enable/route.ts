e04844aa1b7f1adf9c83199e5fafa0fce1fea9de
Dev Shrivastava, Aditya <a.dev.shrivastava@accenture.com>
2025-07-30 18:39:18 +0530

    Exporting runtime package as edge

--- src/app/api/draft-mode/enable/route.ts
+++ src/app/api/draft-mode/enable/route.ts
@@ -1,4 +1,5 @@
 // src/app/api/draft-mode/enable/route.ts
+export const runtime = "edge";
 
 import { sanityClient } from "@/sanity/client";
 import { defineEnableDraftMode } from "next-sanity/draft-mode";
